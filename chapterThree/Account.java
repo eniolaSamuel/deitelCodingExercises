0f6b53810727fd74c7d7547b9eb35aa3375dfbea
Eniola Samuel <theeniolasamuel@gmail.com>
2022-11-10 17:12:22 +0100

    initial commit

---
+++ chapterThree/Account.java
@@ -0,0 +1,24 @@
+public class Account {
+    private int balance;
+
+    public void deposit(int amount) {
+
+        if (amount > 0) {
+            balance = balance + amount;
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdrawal(int amount) {
+
+        if (amount < balance && amount > 0)
+            balance = balance - amount;
+        if (balance == amount)
+            balance = 0;
+
+    }
+
+}
